1b18f7731b5223a04c0547bdb945bbaa96ed5cea
Aakash-Laptop <aakash200394@gmail.com>
2026-02-11 12:07:42 +0530

    Added order tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { Tracking } from './pages/Tracking'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element = { <HomePage /> } />
-      <Route path="checkout" element= {<CheckoutPage />} />
+      <Route path="checkout" element = {<CheckoutPage />} />
+      <Route path='orders' element = {<OrdersPage /> } />
+      <Route path='tracking' element = {<Tracking />} />
     </Routes>
   )
 }
