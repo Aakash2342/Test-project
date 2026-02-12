38f4dd8f99741b0d570b64365021b8c1b3e2ddb5
Aakash-Laptop <aakash200394@gmail.com>
2026-02-12 11:28:40 +0530

    add server proxy config for /api and /images

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,11 +7,12 @@ export function HomePage() {
     const [products, setProduct] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        // used server proxy for this see vite.config in there see server proxy api
+        axios.get('/api/products')
             .then((response) => {
                 setProduct(response.data);
             })
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then((response) => {
                 setCart(response.data);
             })
